b02ae7d6faac9e96898928b4e102cf99fa8bb230
xiaoLi <75558996+xwtlt@users.noreply.github.com>
2023-01-17 07:11:32 +0000

    Pending changes exported from your codespace

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,11 @@
+export default {
+    // These are app level configs.
+    lang: 'zh-CN',
+    title: '卡赫米兹',
+    description: '欢迎来到卡赫米兹',
+    appearance: true,
+    ignoreDeadLinks: true,
+    lastUpdated: true
+      
+  }
+  
